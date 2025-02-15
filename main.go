328fa3a0da6a2b139a6608763937aa0e19684ab5
Daniil Benger <torigetz@yandex.ru>
2025-02-15 10:30:09 +0300

    feat: add some features

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const IMTPower = 2
+
 func main() {
-	const IMTPower = 2
 	var height float64
 	var kg float64
 	fmt.Println("__ IMT Calculator __")
@@ -14,11 +15,16 @@ func main() {
 	fmt.Scan(&height)
 	fmt.Print("Введите вес:")
 	fmt.Scan(&kg)
-	IMT := kg / math.Pow(height, IMTPower)
-	OutputResult(IMT)
+	imt := CalculateIMT(height, kg)
+	OutputResult(imt)
 }
 
 func OutputResult(imt float64) {
 	result := fmt.Sprintf("ИМТ: %.0f", imt)
 	fmt.Print(result)
+}
+
+func CalculateIMT(userHeight float64, userKg float64) float64 {
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	return IMT
 }
